3b67fe63e9ef32468eab1bb87f9e96be9f148f96
yulin <byteswarning@gmail.com>
2017-08-30 17:27:10 +0800

    add_master: make the pip package

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -1,7 +1,6 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-import copy
 from json import dumps
 from os import environ as env
 import os
@@ -12,6 +11,8 @@ from uuid import uuid4
 
 from requests import get, post
 import yaml
+
+from expand import expand
 
 """
 监控gunicorn启动的web server
@@ -25,85 +26,6 @@ import yaml
     - 全局变量 is_alive 设置为 False 直到服务启动
 服务重新启动后 重新发送注册请求和心跳包 is_alive 设置为 True
 """
-
-
-def traverseDict(d):
-    if type(d) == dict:
-        for k, v in d.items():
-            if k == "$ref":
-                return False
-            else:
-                if not traverseDict(v):
-                    return False
-    return True
-
-
-def convertRef(d, ref):
-    if type(d) != dict:
-        return d
-    if "$ref" in d.keys():
-        url = d["$ref"]
-        url = url.split("/")[-1]
-        if url in ref.keys():
-            return ref[url]
-    else:
-        for k, v in d.items():
-            d[k] = convertRef(v, ref)
-        return d
-
-
-def convertDefinitions(definitions, ref=None):
-    ret = definitions
-    while not traverseDict(definitions):
-        keys = definitions.keys()
-        remains = []
-        for k in keys:
-            if not traverseDict(definitions[k]):
-                remains.append(k)
-        for k in remains:
-            if ref is None:
-                ref = ret
-            ret[k] = convertRef(definitions[k], ref)
-    return ret
-
-
-def convertProperties(paths):  # noqa
-    for key, value in paths.items():
-        for method in ["post", "get", "put", "head", "options", "delete"]:
-            try:
-                params = value[method]["parameters"]
-                properties = []
-                delete = set()
-                for i in range(len(params)):
-                    if params[i]["in"] != "body":
-                        continue
-                    required = False
-                    if "required" in params[i]:
-                        required = params[i]["required"]
-                    r_l = []
-                    if required in params[i]["schema"]:
-                        r_l = params[i]["schema"]["required"]
-                    for name, spec in \
-                            params[i]["schema"]["properties"].items():
-                        sspec = copy.deepcopy(spec)
-                        sspec.update(
-                            {
-                                "in": "body",
-                                "required": required and (name in r_l),
-                                "name": name,
-                            }
-                        )
-                        properties.append(sspec)
-                        delete.add(i)
-            except KeyError:
-                continue
-            l = [i for i in delete]
-            l.reverse()
-            for i in l:
-                params.pop(i)
-            for p in properties:
-                params.append(p)
-    return paths
 
 
 class UsherClientConfig(object):
@@ -135,22 +57,7 @@ class UsherClientConfig(object):
         self.api_origin_spec = dumps(yaml.load(yaml_file))
 
         j = yaml.load(yaml_file)
-        if "definitions" in j and j["definitions"]:
-            definitions = j["definitions"]
-            j["definitions"] = convertDefinitions(definitions)
-            for url in j["paths"]:
-                for method in j["paths"][url]:
-                    try:
-                        list_p = j["paths"][url][method]["parameters"]
-                    except:
-                        continue
-                    for i in range(len(j["paths"][url][method]["parameters"])):
-                        if "$ref" in list_p[i]:
-                            list_p[i] = j[
-                                "parameters"][list_p[i]["$ref"].split("/")[-1]]
-                        else:
-                            list_p[i] = convertDefinitions(
-                                list_p[i], j["definitions"])
+        j = expand(j)
         self.api_spec = dumps(j)
 
     @property
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="usher-heartbeat",
+    version="0.1",
+    description="register and keep heartbeat for backend app",
+    packages=["heartbeat"],
+    scripts=["bin/heartbeat"],
+    install_requires=[
+        "pyyaml",
+        "requests",
+    ],
+)
